66f0fe316b1a423670851c4f435c47c7ee04e80e
Jeanie Ahn <Jeanie.ahn9@gmail.com>
2021-05-19 23:02:58 +0200

    index with styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'DB cleaned'
+puts 'Creating Flats...'
+
+10.times do
+  flat = Flat.create!(
+    name: Faker::Ancient.primordial,
+    address: Faker::Address.street_name,
+    description: Faker::Restaurant.description,
+    price_per_night: Faker::Number.number(digits: 3),
+    number_of_guests: Faker::Number.between(from: 1, to: 10)
+    )
+  flat.save!
+end
+
+puts 'Seeds created!'
